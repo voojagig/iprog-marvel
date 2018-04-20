7e909eefdea650b50ee266f324af84f1e2617d85
sofiaJagerstrom <sofia@jagerstrom.eu>
2018-04-20 11:35:14 +0200

    Navbar test

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './Welcome.css';
+import Navbar from '../Navbar/Navbar';
+
 import { Link } from 'react-router-dom';
 
 class Welcome extends Component {
   render() {
     return (
       <div className="Welcome">
+      <Navbar>
         <p>
             Welcome to the dinner planner React Startup code!
         </p>
