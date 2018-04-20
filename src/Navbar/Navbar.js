238893aad6ea499a10d8edafe67802181012930a
sofiaJagerstrom <sofia@jagerstrom.eu>
2018-04-20 12:22:11 +0200

    Navbar almost work.

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,61 +1,48 @@
 import React, { Component } from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, nav} from 'react-router-dom';
 
-class Welcome extends Component {
+class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-    	<nav class="navbar navbar-default navbar-static-top">
- 			<div class="container-fluid">
-	 			<div class="navbar-header">
-			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-			        <span class="sr-only">Toggle navigation</span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
+    	<nav className="navbar navbar-default navbar-static-top">
+ 			<div className="container-fluid">
+	 			<div className="navbar-header">
+			      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span className="sr-only">Toggle navigation</span>
+			        <span className="icon-bar"></span>
+			        <span className="icon-bar"></span>
+			        <span className="icon-bar"></span>
 			      </button>
-			      <a class="navbar-brand" href="#">Brand</a>
+			      <a className="navbar-brand" href="#">Marvel</a> {/* should be the header */}
 			    </div>
 
-			    <!-- Collect the nav links, forms, and other content for toggling -->
-			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			      <ul class="nav navbar-nav">
-			        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-			        <li><a href="#">Link</a></li>
-			        <li class="dropdown">
-			          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-			          <ul class="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" class="divider"></li>
-			            <li><a href="#">Separated link</a></li>
-			            <li role="separator" class="divider"></li>
-			            <li><a href="#">One more separated link</a></li>
-			          </ul>
-			        </li>
+			    <div className="collapse navbar-collapse" id="bss-example-navbar-collapse-1">
+			      <ul className="nav navbar-nav">
+			        <li className="active"><a href="#">Comics <span className="sr-only">(current)</span></a></li>
+			        <li><a href="#">Characters</a></li>
+			        <li><a href="#">Quiz</a></li>
 			      </ul>
-			      <form class="navbar-form navbar-left">
-			        <div class="form-group">
-			          <input type="text" class="form-control" placeholder="Search">
+			      <form className="navbar-form navbar-left">
+			        <div className="form-group">
+			          <input type="text" className="form-control" placeholder="Search for comics or characters"/>
 			        </div>
-			        <button type="submit" class="btn btn-default">Submit</button>
+			        <button type="submit" className="btn btn-default">Search</button>
 			      </form>
-			      <ul class="nav navbar-nav navbar-right">
-			        <li><a href="#">Link</a></li>
-			        <li class="dropdown">
-			          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-			          <ul class="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" class="divider"></li>
-			            <li><a href="#">Separated link</a></li>
+			      <ul className="nav navbar-nav navbar-right">
+			        <li className="dropdown">
+			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Register<span className="caret"></span></a>
+			          <ul className="dropdown-menu">
+			            <li><a href="#">Log in/Register</a></li>
+			            <li><a href="#">Highscore</a></li>
+			            <li><a href="#">Saved characters</a></li>
+			            <li role="separator" className="divider"></li>
+			            <li><a href="#">Log out</a></li>
 			          </ul>
 			        </li>
 			      </ul>
-			    </div><!-- /.navbar-collapse -->
+			    </div>
 
 
   			</div>
--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -8,14 +8,12 @@ class Welcome extends Component {
   render() {
     return (
       <div className="Welcome">
-      <Navbar>
+      <Navbar/>
         <p>
-            Welcome to the dinner planner React Startup code!
+            Welcome to our Marvel Quiz, to the right you can log in or create an account to save your progress in the game. An registered user can also save their favorite characters. 
+            In the menu above you can start a quiz or browse between Marvels comics and characters to find fun info about them. Enjoy! 
         </p>
-        
-        <Link to="/search">
-            <button>Start planning</button>
-        </Link>
+
       </div>
     );
   }
