37219117e23ebeb33b02757c7ba453b3e89ed893
lindelindblad <lindelindblad@gmail.com>
2018-05-16 17:00:25 +0200

    bilder + namn-array till quiz på plats

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,26 +1,137 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './Quiz.css';
 import Navbar from '../Navbar/Navbar';
-
+import { modelInstance } from '../data/Model';
 import { Link } from 'react-router-dom';
+var shuffle = require('shuffle-array');
+
+
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
         title: 'MarvelQuiz',
-        type: '/quiz'
-
-
+        type: '/quiz', 
+        results: '',
+        status: 'INITIAL',
     };
+  }
+
+  loadData(){
+        modelInstance.getCharacters().then(Data => {
+          const result = Data.data.results.filter((c) => {
+            if( c.thumbnail.path !=="http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available") return true
+          });
+          this.setState({
+            status: 'LOADED',
+            results: result,
+          });
+          // console.log("id: " + this.state.results[0].id);
+          // console.log(this.state.status)
+
+          var nameList = [];
+          for (let i = 0; i < this.state.results.length; i++) {
+            nameList.push(this.state.results[i].name);
+          }
+          shuffle(nameList);
+
+          for (let i = 0; i < this.state.results.length; i++) {
+            console.log("namn: " + nameList[i]);
+          }
+
+          this.setState({name: nameList});
+          // console.log(nameList);
+          //console.log("state: " + this.state.name[0]);
+
+        }).catch(() => {
+          this.setState({
+            status: 'ERROR'
+          });
+
+        });
+
 
   }
+
+  componentDidMount = () => {
+    // when data is retrieved we update the state
+    // this will cause the component to re-render
+    modelInstance.addObserver(this)
+    this.loadData();
+
+  }
+
+
+  // this is called when component is removed from the DOM
+  // good place to remove observer
+  componentWillUnmount() {
+    modelInstance.removeObserver(this)
+  }
+
+  // in our update function we modify the state which will
+  // cause the component to re-render
+  update() {
+    //this.loadData();
+  }
+
+
+
+
   render() {
+    let charactersList = null
+    var nameList =[];
+    
+
+     switch (this.state.status) {
+      case 'INITIAL':
+        charactersList = <em>Choose a letter to see comics</em>
+        break;
+      case 'LOADING':
+        charactersList = <em>Loading...</em>
+        break;
+      case 'LOADED':
+        
+          charactersList = this.state.results.map((comic) =>
+                <div className="col-md-3 col-sm-4" key={comic.id}>
+
+                    <Button>
+
+                    <div className="thumb">
+                      <img src={comic.thumbnail.path + "/portrait_fantastic." + comic.thumbnail.extension} alt=""/>
+                      <div className="caption">
+                        <h4>{comic.name}</h4>
+                      </div>
+                    </div>
+
+                    </Button>
+
+                </div>
+              );
+
+          break;
+      default:
+        charactersList = <b>Failed to load data, please try again. Verify that you have network connection, please. </b>
+        break;
+    }
+
     return (
-      <div className="Welcome">
-      <Navbar location = {this.state.type}/>
-<p>det här är quiz </p>
-      </div>
+      <div className="container Quiz">
+        <Navbar location = {this.state.type}/>
+        <h1>det här är quiz </h1>
+         <div className="row">
+               
+        </div>
+
+        <div className="row">
+          {charactersList}
+        </div>
+       
+        
+
+        </div>
+
     );
   }
 }
--- src/data/Model.js
+++ src/data/Model.js
@@ -28,7 +28,7 @@ const Model = function() {
 		 .catch(handleError)
 	}
 
-	this.getComic = function(characterId) {
+	this.getComic = function(comicId) {
 		console.log("hejsan inne i getComic");
 		//comics?titleStartsWith=A&orderBy=title&apiKey...
 		var PRIV_KEY = '1a60651bb50c75bb1aa84ede4cdfd872bf409040';
@@ -44,9 +44,35 @@ const Model = function() {
 
 		//we have trouble of getting the data from the API.
 		var url = 'http://gateway.marvel.com:80/v1/public/comics/';
-		var finalurl = url + characterId + '?' + 'ts=' + ts + '&apikey=' + PUBLIC_KEY + '&hash=' + hash;
+		var finalurl = url + comicId + '?' + 'ts=' + ts + '&apikey=' + PUBLIC_KEY + '&hash=' + hash;
 
 		// example url: http://gateway.marvel.com/v1/public/comics/ID?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150 
+		console.log(finalurl);
+
+		 return fetch(finalurl)
+		 .then(processResponse)
+		 .catch(handleError)
+	}
+
+
+	this.getCharacters = function() {
+		//comics?titleStartsWith=A&orderBy=title&apiKey...
+		var PRIV_KEY = '1a60651bb50c75bb1aa84ede4cdfd872bf409040';
+		var PUBLIC_KEY = '988fc225729038dfd5246cb095fcc5ec';
+
+		// you need a new ts every request                                                                                    
+		var ts = new Date().getTime();
+		var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+		  
+		// the api deals a lot in ids rather than just the strings you want to use
+		//var characterId = '1009718'; // wolverine  
+		//var series = 'series=' + 'avengers';                                                                           
+
+		//we have trouble of getting the data from the API.
+		var url = 'http://gateway.marvel.com:80/v1/public/characters?';
+		var finalurl = url + 'limit=8&' + 'ts=' + ts + '&apikey=' + PUBLIC_KEY + '&hash=' + hash;
+
+		// example url: http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150 
 		console.log(finalurl);
 
 		 return fetch(finalurl)
@@ -68,10 +94,10 @@ const Model = function() {
 	const handleError = function (error) {
 	    if (error.json) {
 	      error.json().then(error => {
-	        console.error('getAllDishes() API Error:', error.message || error)
+	        console.error('getsomething() API Error:', error.message || error)
 	      })
 	    } else {
-	      console.error('getAllDishes() API Error:', error.message || error)
+	      console.error('getsomething() API Error:', error.message || error)
 	    }
 	    throw error;
   	}
