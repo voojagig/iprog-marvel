7391b9ae2be34fe2a678ef9da0565a08661214f0
lindelindblad <lindelindblad@gmail.com>
2018-05-16 10:50:30 +0200

    update comicmodal

--- src/ComicModal/ComicModal.js
+++ src/ComicModal/ComicModal.js
@@ -1,25 +1,39 @@
 import { Button } from 'react-bootstrap';
 import { ButtonToolbar } from 'react-bootstrap';
-import Modal from 'react-bootstrap/lib/Modal'
-import ModalHeader from 'react-bootstrap/lib/ModalHeader';
+import { Modal } from 'react-bootstrap/lib/Modal'
+import { ModalHeader } from 'react-bootstrap/lib/ModalHeader';
 
 class ComicModal extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // We create the state to store the various statuses
+    // e.g. API data loading or error 
+    //alert("inuti consructor");
+    this.state = {
+      show: this.props.show,
+      comic: this.props.comic,
+    };
+  }
+
   render() {
     return (
-      <Modal show={this.props.show} onHide={this.props.handleClose} aria-labelledby="ModalHeader"
->    
-
+      <Modal show={this.state.show} onHide={this.handleClose} aria-labelledby="ModalHeader">          
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
+          <Modal.Title id='ModalHeader'>A Title Goes here</Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          <h4>Wrapped Text</h4>
-         
+          <p>Some Content here</p>
         </Modal.Body>
+
         <Modal.Footer>
-          <Button>Close</Button>
+          // If you don't have anything fancy to do you can use
+          // the convenient `Dismiss` component, it will
+          // trigger `onHide` when clicked
         </Modal.Footer>
       </Modal>
+  
     );
   }
 }
